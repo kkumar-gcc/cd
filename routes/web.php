615f70c7ba0fdc197dd8de7f432f79751a9e9a5c
krishan kumar <84431594+kkumar-gcc@users.noreply.github.com>
2023-02-24 17:18:05 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -28,21 +28,6 @@ use Illuminate\Support\Facades\Auth;
 
 Route::get('/', [HomeController::class, 'home'])->name('home');
 
-Route::get('/email/verify', function () {
-    return view('auth.verify-email');
-})->middleware('auth')->name('verification.notice');
-
-Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-    $request->fulfill();
-    return redirect('/home');
-})->middleware(['auth', 'signed'])->name('verification.verify');
-
-Route::post('/email/verification-notification', function (Request $request) {
-    $request->user()->sendEmailVerificationNotification();
-
-    return back()->with('message', 'Verification link sent!');
-})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
-
 
 Route::get('/about', function () {
     return view('about');
